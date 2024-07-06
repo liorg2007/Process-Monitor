5795e442011f0259c8c6a9ae0ee59bb30c047725
Lior G <liorgleizer1@gmail.com>
2024-07-06 14:13:19 +0300

    changed to 2 space indent

--- ProcessMonitor/ProcessRetriever.cpp
+++ ProcessMonitor/ProcessRetriever.cpp
@@ -3,109 +3,108 @@
 //
 
 #include "ProcessRetriever.h"
-#include <algorithm>
 
 
 ProcessRetreiver::ProcessRetreiver()
- : memTotal_(GetMemTotal()) {
+  :memTotal_(GetMemTotal()) {
 
 }
 
 std::vector<Process> ProcessRetreiver::GetRunningProcesses() {
-    std::vector<Process> processes;
+	std::vector<Process> processes;
 
-    for(const auto& entry : std::filesystem::directory_iterator(PROC_DIR_PATH)) {
-        if(!IsNumeric(entry.path().filename().string()) ||
-            !entry.is_directory())
-            continue;
+	for(const auto& entry : std::filesystem::directory_iterator(PROC_DIR_PATH)) {
+		if(!IsNumeric(entry.path().filename().string()) ||
+			!entry.is_directory())
+			continue;
 
-        std::ifstream statusFile(entry.path().string() + STATUS_FILE_PATH);
+		std::ifstream statusFile(entry.path().string() + STATUS_FILE_PATH);
 
-        std::string procName = GetProcessName(statusFile);
-        processes.emplace_back(
-            std::stoi(entry.path().filename().string()),
-            procName,
-            GetProcessCPU_Usage(entry),
-            GetProcessMemoryUsage(statusFile));
+		std::string procName = GetProcessName(statusFile);
+		processes.emplace_back(
+			std::stoi(entry.path().filename().string()),
+			procName,
+			GetProcessCPU_Usage(entry),
+			GetProcessMemoryUsage(statusFile));
 
-    }
-    return processes;
+	}
+	return processes;
 }
 
 bool ProcessRetreiver::IsNumeric(const std::string_view &string) const {
-    for(const auto& letter : string) {
-        if(!std::isdigit(letter))
-            return false;
-    }
+	for(const auto& letter : string) {
+		if(!std::isdigit(letter))
+			return false;
+	}
 
-    return true;
+	return true;
 }
 
 int ProcessRetreiver::GetMemTotal() const {
-    std::ifstream meminfoFile(MEMINFO_FILE_PATH);
-    std::string firstLine;
+	std::ifstream meminfoFile(MEMINFO_FILE_PATH);
+	std::string firstLine;
 
-    if(!meminfoFile.is_open())
-        throw std::runtime_error(MEMINFO_FILE_ERROR);
+	if(!meminfoFile.is_open())
+		throw std::runtime_error(MEMINFO_FILE_ERROR);
 
-    std::getline(meminfoFile, firstLine); //The MemTotal field is in the first line
+	std::getline(meminfoFile, firstLine); //The MemTotal field is in the first line
 
-    return GetIntegerValFromLine(firstLine);
+	return GetIntegerValFromLine(firstLine);
 }
 
 std::string ProcessRetreiver::GetProcessName(std::ifstream &statusFile) const {
-    std::string firstLine;
-    std::getline(statusFile, firstLine);
-    return GetStringValFromLine(firstLine);
+	std::string firstLine;
+	std::getline(statusFile, firstLine);
+	return GetStringValFromLine(firstLine);
 }
 
 double ProcessRetreiver::GetProcessMemoryUsage(std::ifstream &statusFile) const {
-    std::string line;
-    std::getline(statusFile, line);
+	std::string line;
+	std::getline(statusFile, line);
 
-    while(!line.starts_with(VM_RSS_KEY)) {
-        if(statusFile.eof())
-            return 0;
-        std::getline(statusFile, line);
-    }
+	while(!line.starts_with(VM_RSS_KEY)) {
+		if(statusFile.eof())
+			return 0;
+		std::getline(statusFile, line);
+	}
 
-    return GetIntegerValFromLine(line) / static_cast<double>(memTotal_) * 100;
+	return GetIntegerValFromLine(line) / static_cast<double>(memTotal_) * 100;
 }
 
 double ProcessRetreiver::GetProcessCPU_Usage(const std::filesystem::directory_entry &dir)  {
-    std::ifstream statFile(dir.path().string() + STAT_FILE_PATH);
-    std::string line;
-    std::getline(statFile, line);
-    double utime, stime, startTime;
-    std::string trashValue;
+	std::ifstream statFile(dir.path().string() + STAT_FILE_PATH);
+	std::string line;
+	std::getline(statFile, line);
+	double utime, stime, startTime;
+	std::string trashValue;
 
-    std::stringstream ss(line);
+	std::stringstream ss(line);
 
-    for(int i = 0; i<UTIME_INDEX; i++)
-        ss >> trashValue;
+	for(int i = 0; i<UTIME_INDEX; i++)
+		ss >> trashValue;
 
-    ss >> utime;
-    ss >> stime; //stime is one index after utime
+	ss >> utime;
+	ss >> stime; //stime is one index after utime
 
-    for(int i = UTIME_INDEX + 1; i<START_TIME_INDEX; i++)
-        ss >> trashValue;
+	for(int i = UTIME_INDEX + 1; i<START_TIME_INDEX; i++)
+		ss >> trashValue;
 
-    ss >> startTime;
+	ss >> startTime;
 
-    //clock tick into seconds
-    utime /= CLK_TCK;
-    stime /= CLK_TCK;
-    startTime /= CLK_TCK;
-    double upTime = GetSystemUpTime();
-    return (utime + stime) * 100 / (GetSystemUpTime() - startTime);
+	//clock tick into seconds
+	utime /= CLK_TCK;
+	stime /= CLK_TCK;
+	startTime /= CLK_TCK;
+	double upTime = GetSystemUpTime();
+	return (utime + stime) * 100 / (GetSystemUpTime() - startTime);
 }
 
 double ProcessRetreiver::GetSystemUpTime() {
-    std::ifstream uptimeFile(UPTIME_DIR_PATH);
-    std::string line;
-    std::getline(uptimeFile, line);
+	std::ifstream uptimeFile(UPTIME_DIR_PATH);
+	std::string line;
+	std::getline(uptimeFile, line);
 
-    return std::stod(line.substr(0, line.find_first_of(" "))); //first number in the uptime file
+	return std::stod(line.substr(0, line.find_first_of(" "))); //first number in the uptime file
 }
 
 /*
@@ -115,11 +114,11 @@ Key:    value
 amount of spaces is unknown
  */
 std::string ProcessRetreiver::GetStringValFromLine(const std::string &line) const {
-    return line.substr(line.find_last_of("\t") + 1);
+	return line.substr(line.find_last_of("\t") + 1);
 }
 
 int ProcessRetreiver::GetIntegerValFromLine(const std::string &line) const {
-    return std::stoi(line.substr(line.find_first_of(DIGITS), line.find_last_of(DIGITS)));
+	return std::stoi(line.substr(line.find_first_of(DIGITS), line.find_last_of(DIGITS)));
 }
 
 
--- ProcessMonitor/ProcessRetriever.h
+++ ProcessMonitor/ProcessRetriever.h
@@ -11,50 +11,51 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 #include <filesystem>
 
 struct Process {
-    const pid_t PID;
-    const std::string name;
-    const double CPU_Usage;
-    const double memUsage;
+	const pid_t PID;
+	const std::string name;
+	const double CPU_Usage;
+	const double memUsage;
 
-    Process(const pid_t PID, const std::string& name, const double CPU_Usage, const double memUsage)
-        :PID(PID), name(name), CPU_Usage(CPU_Usage), memUsage(memUsage)
-    {}
+	Process(const pid_t PID, const std::string& name, const double CPU_Usage, const double memUsage)
+		:PID(PID), name(name), CPU_Usage(CPU_Usage), memUsage(memUsage)
+	{}
 };
 
 class ProcessRetreiver
 {
 public:
-    ProcessRetreiver();
+	ProcessRetreiver();
 
-    [[nodiscard]] std::vector<Process> GetRunningProcesses();
+	[[nodiscard]] std::vector<Process> GetRunningProcesses();
 
 private:
-    static constexpr auto DIGITS = "0123456789";
-    static constexpr auto MEMINFO_FILE_PATH = "/proc/meminfo";
-    const std::filesystem::path PROC_DIR_PATH{"/proc"};
-    static constexpr auto STATUS_FILE_PATH = "/status";
-    static constexpr auto STAT_FILE_PATH = "/stat";
-    static constexpr auto UPTIME_DIR_PATH = "/proc/uptime";
-    static constexpr auto VM_RSS_KEY = "VmRSS";
-    static constexpr auto CLK_TCK = 100;
-    static constexpr auto UTIME_INDEX = 13;
-    static constexpr auto START_TIME_INDEX = 20;
+	static constexpr auto DIGITS = "0123456789";
+	static constexpr auto MEMINFO_FILE_PATH = "/proc/meminfo";
+	const std::filesystem::path PROC_DIR_PATH{"/proc"};
+	static constexpr auto STATUS_FILE_PATH = "/status";
+	static constexpr auto STAT_FILE_PATH = "/stat";
+	static constexpr auto UPTIME_DIR_PATH = "/proc/uptime";
+	static constexpr auto VM_RSS_KEY = "VmRSS";
+	static constexpr auto CLK_TCK = 100;
+	static constexpr auto UTIME_INDEX = 13;
+	static constexpr auto START_TIME_INDEX = 20;
 
-    static constexpr auto MEMINFO_FILE_ERROR = "Can't open /proc/meminfo";
+	static constexpr auto MEMINFO_FILE_ERROR = "Can't open /proc/meminfo";
 
-    [[nodiscard]] bool IsNumeric(const std::string_view& string) const;
-    [[nodiscard]] int GetMemTotal() const;
-    [[nodiscard]] std::string GetProcessName(std::ifstream& statusFile) const;
-    [[nodiscard]] double GetProcessMemoryUsage(std::ifstream& statusFile) const;
-    [[nodiscard]] double GetProcessCPU_Usage(const std::filesystem::directory_entry& dir);
-    [[nodiscard]] double GetSystemUpTime();
-    std::string GetStringValFromLine(const std::string& line) const;
-    int GetIntegerValFromLine(const std::string& line) const;
+	[[nodiscard]] bool IsNumeric(const std::string_view& string) const;
+	[[nodiscard]] int GetMemTotal() const;
+	[[nodiscard]] std::string GetProcessName(std::ifstream& statusFile) const;
+	[[nodiscard]] double GetProcessMemoryUsage(std::ifstream& statusFile) const;
+	[[nodiscard]] double GetProcessCPU_Usage(const std::filesystem::directory_entry& dir);
+	[[nodiscard]] double GetSystemUpTime();
+	std::string GetStringValFromLine(const std::string& line) const;
+	int GetIntegerValFromLine(const std::string& line) const;
 
-    const int memTotal_;
+	const int memTotal_;
 };
 
 
